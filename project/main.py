ab322c8f628f7ef7e35f426c73ba315a21143876
Batsheva Rich <batsheva7424@gmail.com>
2020-06-18 16:14:24 +0300

    play songs with mixer

--- project/main.py
+++ project/main.py
@@ -1,5 +1,12 @@
-import playsound
+import pygame
+import time
 import os
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.mixer.init()
 for filename in os.listdir(r'C:\Users\owner\Music'):
-    playsound.playsound(
+    pygame.mixer.music.load(
         os.path.join(r'C:\Users\owner\Music', filename))
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(1000.0)
+    time.sleep(30)
+    pygame.mixer.music.stop()
